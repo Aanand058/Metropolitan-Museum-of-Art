460a1c7892acb12ace42890447b2ccb02dc9cee0
Aanand Aman <109314074+Aanand058@users.noreply.github.com>
2023-12-12 05:48:49 +0000

    Final Push

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,4 @@
 
-/*********************************************************************************
-*  WEB422 – Assignment 6
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: __Aanand Aman___ Student ID: __166125211_____ Date: 2023/11/29
-*
-*
-********************************************************************************/ 
-
-
-
-
-//Used Assignment 4 given by Professor
 
 import { Image, Row, Col } from "react-bootstrap";
 
